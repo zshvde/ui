17cb75e43a3d0cfc5d32176b702525e9958918ba
zshvde <92206542+zshvde@users.noreply.github.com>
2024-04-16 23:32:30 +0200

    Update orion.lua

--- orion.lua
+++ orion.lua
@@ -3,7 +3,7 @@
 local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
-local LocalPlayer = game:GetService("Players")).LocalPlayer
+local LocalPlayer = game:GetService("Players").LocalPlayer
 local Mouse = LocalPlayer:GetMouse()
 local HttpService = game:GetService("HttpService")
 
